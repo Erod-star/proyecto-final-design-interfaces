da0861623087a79b925aea0f867c875e6888f585
Edson <erodstardev@gmail.com>
2022-11-29 21:58:35 -0600

    Add awesomeCard component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="about">
     <h1>This is an about page</h1>
-    <HelloWorld />
+    <!-- <HelloWorld /> -->
+    <AwesomeCard />
     <button type="button" class="btn btn-info" @click="navigateToHome">
       Navigate to the home page!
     </button>
@@ -10,13 +11,15 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import AwesomeCard from "@/components/awesome-card/AwesomeCard.vue";
 import router from "@/router";
 
 export default defineComponent({
   name: "AboutView",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    AwesomeCard,
   },
   setup() {
     return {
@@ -26,3 +29,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
